9091e412702def7c4c745b5e130e21dd5ff2c768
Mathew <M.beale@me.com>
2015-06-28 17:35:30 +0100

    Saves greeting over mutiple pages & validates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,33 +20,46 @@ angular.module('angularQuizApp')
     ];
   })
 
+  .factory('StorageService',[
+    function(){
+
+      var _name = "";
+
+      var _setName  = function(name){
+        _name = name;
+        return "Let's start the quiz!";
+      };
+
+      var _getName = function(){
+        return _name;
+      };
+
+      return {
+        setName : _setName,
+        getName : _getName
+      };
+
+    }])
+
+  .controller('MainCtrl', ['$scope','$route', '$location',"StorageService",
+    function($scope, $route, $location, StorageService) {
+
+      $scope.name = StorageService.getName();
+
+      $scope.valueReturned = "";
+
+      $scope.saveMyName = function(name){
+        var result = StorageService.setName(name);
+        $scope.valueReturned =angular.copy(result);
+
+        //Update name to the view
+        $scope.name = StorageService.getName();
+      };
+
+    }])
 
 
-.controller('MyController', ['$scope', 'StorageService',
-  function($scope, StorageService) {
-    $scope.name = StorageService.getName(); // or setName(name_value)
-  }])
-
-
-.factory('StorageService',[
-  function(){
-
-    var name = "";
-
-    var _setName  = function(x){
-      this.name = x;
-    };
-
-    var _getName = function(){
-      return name;
-    };
-
-    return {
-      setName : _setName,
-      getName : _getName
-    }
 
 
 
-  }]);
 
